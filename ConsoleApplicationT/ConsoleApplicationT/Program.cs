e8bf1dbfc498117dcb46583aa8e9194c717f1f6f
kyojujor <524810052@qq.com>
2015-11-23 23:18:18 +0800

    ~hello world

---
+++ ConsoleApplicationT/ConsoleApplicationT/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using TestClass;
+
+namespace ConsoleApplicationT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = typeof(TestClass.TestClass);
+
+            var Methods = t.GetMethods();
+            t.GetProperties();
+
+            foreach (var item in Methods)
+            {
+
+            }
+
+            //var method = t.GetMethod("Function1");
+            var method1= t.GetMethod("Function1", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static, null, new Type[] { }, null);
+            var method2 = t.GetMethod("Function2");
+            var method = t.GetMethod("Function2", BindingFlags.Public|BindingFlags.NonPublic | BindingFlags.Static);
+            string saf = (string)method2.Invoke(null,new object[] {
+                "12"
+            });
+            //var za = t.GetProperties()
+            Console.WriteLine(saf);
+
+            Console.ReadKey();
+        }
+
+        public static void a()
+        {
+            var a = typeof(TestClass.TestClass);
+
+            var Methods = a.GetMethods();
+        }
+    }
+}
---
+++ ConsoleApplicationT/TestClass/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 测试用类
+/// </summary>
+namespace TestClass
+{
+    public class TestClass
+    {
+        public static string Function1()
+        {
+            return "方法1";
+        }
+
+        public static string Function2(int i)
+        {
+            return "方法2";
+        }
+
+    }
+}
